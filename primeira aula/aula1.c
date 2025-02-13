88c4bfe645df911b3b305458b7d8fee7a339a4a7
PauloNeto-cloud <pbaneto13@gmail.com>
2025-02-13 11:48:14 +0000

    aula1

--- primeira aula/aula1.c
+++ primeira aula/aula1.c
@@ -3,9 +3,8 @@
 int main(){
     int n1 = 5;
     double n2 = 1.345;
-    char letra = 'a';
-    printf("n1 = %d\n", n1);
-    printf("n2 = %f\n", n2);;
-    printf("letra = %c\n", letra);
+    char letra1 = 'a', letra2 = 'b';  
+    printf("n1 = %d e n2 = %.2f\n", n1, n2);
+    printf("letra = %c e letra2 = %c\n", letra1, letra2);
     return 0;
 }
