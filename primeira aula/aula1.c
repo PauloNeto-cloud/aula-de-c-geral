4a7b24e895ff6d3e035ba9faafac980edf2284ed
PauloNeto-cloud <pbaneto13@gmail.com>
2025-02-13 12:07:00 +0000

    aula1

--- primeira aula/aula1.c
+++ primeira aula/aula1.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    int n1 = 5;
-    double n2 = 1.345;
-    char letra1 = 'a', letra2 = 'b';  
+    int n1;
+    double n2;
+    char letra1 = 'a', letra2 = 'b'; 
+
+    printf("digite um valor inteiro:");
+    scanf("%d", &n1);
+    
+    printf("digite um valor real:");
+    scanf("%lf", &n2);
+     
     printf("n1 = %d e n2 = %.2f\n", n1, n2);
     printf("letra = %c e letra2 = %c\n", letra1, letra2);
     return 0;
