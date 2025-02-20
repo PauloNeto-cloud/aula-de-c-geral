e41e3c41ad8bdc8f3f102dc69806ea62f4924459
PauloNeto-cloud <pbaneto13@gmail.com>
2025-02-20 11:38:11 +0000

    aula2

--- segunda aula/exercio2.c
+++ segunda aula/exercio2.c
@@ -14,16 +14,16 @@ int main() {
     if (imc < 18.5) {
         printf("Abaixo do peso\n");
     } else {
-        if (imc >= 18.5 && imc <= 24.9) {
+        if (imc <= 24.9) {
             printf("Peso normal\n");
     } else {
-        if (imc >= 25 && imc <= 29.9) {
+        if (imc <= 29.9) {
             printf("Sobrepeso\n");
     } else {
-        if (imc >= 30 && imc <= 34.9) {
+        if (imc <= 34.9) {
             printf("Obesidade grau 1\n");
     } else {
-        if (imc >= 35 && imc <= 39.9) {
+        if (imc <= 39.9) {
             printf("Obesidade grau 2\n");
     } else {
         printf("Obesidade grau 3\n");
