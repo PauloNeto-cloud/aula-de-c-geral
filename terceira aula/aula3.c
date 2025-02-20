9e54249ef3ca14c65b3b0ea239fe86ee18063f45
PauloNeto-cloud <pbaneto13@gmail.com>
2025-02-20 19:02:36 +0000

    aula3

--- terceira aula/aula3.c
+++ terceira aula/aula3.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
+#define MAX_REPET 5
 
 int main(){
-    int num1, num2;
+    int num1, num2, qtdRepet = 1;
     printf("digite um numero inteiro: ");
     scanf("%d", &num1);
 
-    printf("digite um numero inteiro maior do que %d\n",num1);
+    printf("digite um numero inteiro maior do que %d: ",num1);
     scanf("%d", &num2);
     
-    while(num2 <= num1){
-        printf("digite um numero maior que %d\n");
+    while(num2 <= num1 && qtdRepet <= MAX_REPET){
+        qtdRepet = qtdRepet + 1;
+        printf("valor digitado e invalido!\n");
+        printf("digite um numero inteiro maior que %d\n", num1);
         scanf("%d", &num2);
     }
-    printf("existem %d valores no intervalo de %d a %d",
-        num1 - num2 + 1, num1, num2);
-        return 0;
-
+    if(qtdRepet <= MAX_REPET){
+        printf("existem %d valores no intervalo de %d a %d",
+            num1 - num2 + 1, num1, num2);
+            return 0;
+    }else{
+        printf("acabou todas as repeticoes!\n");
+        return 1;
+        
+    }
 }
