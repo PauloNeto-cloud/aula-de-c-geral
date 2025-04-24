6a298d6a7cbe03086a4c23826a61cd65834c88d1
PauloNeto-cloud <pbaneto13@gmail.com>
2025-04-24 11:37:25 +0000

    aulas

---
+++ aulas/aulasgeral/reviao_de_vetores/erxecio1.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+#include <stdlib.h>
+
+const int TAM =100;
+
+int main(){
+    int tamvetororig = 0, tamvetorord = 0;
+    int opçao;
+    do{
+    printf("\nmenu\n\n");
+    printf("1- inserir um numero no vetor\n");
+    printf("2- criar vetor ordenado\n");
+    printf("3- busca binaria");
+    
+    }
+}
